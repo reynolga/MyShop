ff4d30148a9a1ab5207f8d7dfc2b620625b97730
Reynolds <xander.reynolds@siemens.com>
2019-07-15 10:44:52 -0400

    Product Filtering is available

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,10 +34,26 @@ namespace MyShop.WebUI.Controllers
          }
       }
 
-      public ActionResult Index()
+      //Category=null means it's optional.
+      public ActionResult Index(string Category=null)
       {
-         List<Product> products = context.Collection().ToList();
-         return View(products);
+         List<Product> products;
+         List<ProductCategory> categories = productCategories.Collection().ToList();
+
+         if (Category == null)
+         {
+            products = context.Collection().ToList();
+         }
+         else
+         {
+            products = context.Collection().Where(p => p.Category == Category).ToList();
+         }
+
+         ProductListViewModel model = new ProductListViewModel();
+         model.Products = products;
+         model.ProductCategories = categories;
+
+         return View(model);
       }
 
       public ActionResult About()
